b419b33338856899e0ee87a50e683e0f68b8ccf8
Htivus <suvithkumar0001@gmail.com>
2023-09-18 19:12:00 +0530

    error

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        MONGO_URI: "mongodb+srv://suvithkumar0001:GfUcpstBG2RFex7I@cluster0.iqertbv.mongodb.net/?retryWrites=true&w=majority"
-  }
 }
 
 module.exports = nextConfig
